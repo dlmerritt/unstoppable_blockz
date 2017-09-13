a0134eb2c7880c8c4203f25311337e22b7b18a8c
dlmerritt <dlmerrit@uci.edu>
2017-09-13 01:46:58 -0700

    Project Tab + Restart Menu
    Cleaned up the project tab and added a restart menu

---
+++ Prototype/Assets/Scripts/RestartMenuScriptController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartMenuScriptController : MonoBehaviour {
+
+	public void OnRestartButtonClick()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OnMenuButtonClick()
+    {
+        SceneManager.LoadScene("menuScene2");
+    }
+}
