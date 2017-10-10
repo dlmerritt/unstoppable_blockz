c1ef8ce076a5d6a6a781033686923a901e2be8d8
Fernando de Paz <f.depaz@hotmail.com>
2017-10-09 18:17:37 -0700

    Before things break

--- Prototype/Assets/Scripts/GameScene/Ball.cs
+++ Prototype/Assets/Scripts/GameScene/Ball.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour {
+public class Ball : MonoBehaviour
+{
 
     public GameObject BallClones;
     public LevelCount GameController;
@@ -11,9 +12,10 @@ public class Ball : MonoBehaviour {
     public Transform resetPos;
     private bool isBreakingStuff;
     private Vector2 landingPosition;
-
+    public LineRenderer line;
     private bool GameOver;
-    public bool gameOver {
+    public bool gameOver
+    {
         get { return GameOver; }
         set { GameOver = value; }
     }
@@ -38,12 +40,15 @@ public class Ball : MonoBehaviour {
         gameOver = false;
         isBreakingStuff = false;
         mInput = GameObject.Find("GameController").GetComponent<MobileInput>();
+        //line.SetPosition(0, Vector3.zero);
+        //line.SetPosition(1, Vector3.zero);
+        //line.SetPosition(2, Vector3.zero);
         //currentSpawnY = 0.45f;
     }
 
     private void Update()
     {
-        
+
         if (!gameOver)
         {
 
@@ -54,10 +59,11 @@ public class Ball : MonoBehaviour {
                 PoolInput();
 
             }
-            if (touchedFloor) {
+            if (touchedFloor)
+            {
                 transform.position = resetPos.position;
                 sprite.enabled = true;
-                
+
             }
             if (CurrentBalls <= 0 && GameObject.FindGameObjectsWithTag("Clone").Length == 0)
             {
@@ -65,7 +71,7 @@ public class Ball : MonoBehaviour {
                 {
 
                     //resetPos.position = transform.position;
-                    
+
                     isBreakingStuff = false;
                 }
                 else
@@ -94,99 +100,164 @@ public class Ball : MonoBehaviour {
             {
                 ballsPreview.parent.gameObject.SetActive(false);
             }
-            else 
+            else
             {
-                Vector3 forward = sd.normalized;
+
+                Vector3 forward = sd.normalized * speed;
                 forward.z = 0;
-                RaycastHit2D h = Physics2D.Raycast(transform.position, forward,LayerMask.GetMask("Walls"));
+                RaycastHit2D h = Physics2D.Raycast(transform.position, forward, LayerMask.GetMask("Walls"));
                 //Debug.DrawRay(transform.position, forward * 15, Color.green);
-                if (h.collider != null) {
-                    Debug.Log(h.collider.name);
-                    Debug.DrawRay(transform.position, h.point, Color.red);
-                }
-                
-
-                //ballsPreview.parent.up = sd.normalized;
-                //ballsPreview.parent.gameObject.SetActive(true);
-                //ballsPreview.localScale = Vector3.Lerp(new Vector3(1, 3, 1), new Vector3(1, 10, 1), sd.magnitude / MAXIMUM_PULL);
-                int totalBricks = GameObject.FindGameObjectsWithTag("Bricks").Length + GameObject.FindGameObjectsWithTag("NewBallBrick").Length;
-                if (mInput.release &&  totalBricks > 0 )
+                if (h.collider != null)
                 {
-                    tutorialContainer.SetActive(false);
-                    isBreakingStuff = true;
-                    //SendBallInDirection(sd.normalized);
-                    currentPos = resetPos.position;
-                    touchedFloor = false;
-                    GetComponent<SpriteRenderer>().enabled = false;
-                    StartCoroutine(ballWait(sd.normalized));
-                    ballsPreview.parent.gameObject.SetActive(false);
+                    //Vector3 forward = sd.normalized * 5;
+                    Vector3 startPoint = transform.position;
+                    line.positionCount = 4;
+
+                    Vector3 reflected = Vector3.Reflect(sd.normalized, h.normal);
+                    //Debug.DrawRay(transform.position, forward, Color.red);
+                    if (h.collider != null) {
+                        //Debug.Log(h.collider.name + " " +h.collider.gameObject.layer);
+                        line.SetPosition(0, transform.position);
+                        line.SetPosition(1, h.point);
+                        startPoint = h.point;
+                        h = Physics2D.Raycast(startPoint, reflected, Mathf.Infinity, 1 << LayerMask.NameToLayer("Walls"));
+
+                        if (h.collider != null)
+                        {
+                            line.SetPosition(2, h.point);
+                            startPoint = h.point;
+                            reflected = Vector3.Reflect(reflected, h.normal);
+                            h = Physics2D.Raycast(startPoint, reflected, Mathf.Infinity, 1 << LayerMask.NameToLayer("Walls"));
+
+                            if (h.collider != null)
+                            {
+                                line.SetPosition(3, h.point);
+                            }
+                        }
+
+                        Debug.DrawRay(transform.position, h.point * 15, Color.green);
+                    }
+
+                    
+                    /*int verts = 100;
+                    line.positionCount = verts;
+
+                    Vector2 startpos = transform.position;
+                    Vector2 vel = sd.normalized * speed;
+                    Vector2 grav = new Vector2(Physics.gravity.x, Physics.gravity.y);
+
+                    for (int i = 0; i < verts; i++)
+                    {
+                        line.SetPosition(i, startpos);
+                        RaycastHit2D ha = Physics2D.Raycast(startpos, sd.normalized, 10, 1 << LayerMask.NameToLayer("Walls"));
+                        if (ha.collider != null)
+                        {
+
+                            startpos = ha.point;
+                            vel = Vector3.Reflect(vel, ha.normal);
+
+                        }
+                        //vel += Physics2D.gravity * Time.fixedDeltaTime;
+                        startpos += vel * Time.fixedDeltaTime;
+
+                        Vector3 forwarda = sd.normalized * 5;
+                        RaycastHit2D he = Physics2D.Raycast(transform.position, -sd.normalized);
+                        Debug.DrawRay(transform.position, forwarda, Color.red);
+                        if (he.collider != null)
+                        {
+
+                            //Debug.DrawRay(transform.position, h.point, Color.green);
+
+                        }
+
+                    }*/
+                        ballsPreview.parent.up = sd.normalized;
+                        ballsPreview.parent.gameObject.SetActive(true);
+                        ballsPreview.localScale = Vector3.Lerp(new Vector3(1, 3, 1), new Vector3(1, 10, 1), sd.magnitude / MAXIMUM_PULL);
+                        int totalBricks = GameObject.FindGameObjectsWithTag("Bricks").Length + GameObject.FindGameObjectsWithTag("NewBallBrick").Length;
+                        if (mInput.release && totalBricks > 0)
+                        {
+                            tutorialContainer.SetActive(false);
+                            isBreakingStuff = true;
+                            //SendBallInDirection(sd.normalized);
+                            currentPos = resetPos.position;
+                            touchedFloor = false;
+                            GetComponent<SpriteRenderer>().enabled = false;
+                            StartCoroutine(ballWait(sd.normalized));
+                            ballsPreview.parent.gameObject.SetActive(false);
+                        }
+
+                    
+
+
+                    //Currently dragging
+
+                    //ballsPreview.localScale = Vector3.Lerp(new Vector3(1, 3, 1), new Vector3(1, 10, 1), sd.magnitude / MAXIMUM_PULL);
                 }
 
+                //Debug.Log(MobileInput.Instance.swipeDelta);
+
+            }
+        }
+    }
+
+            IEnumerator ballWait(Vector3 n)
+            {
+                CurrentBalls = GameController.currentBalls;
+                for (int i = 0; i < GameController.currentBalls; i++)
+                {
+                    int totalBricks = GameObject.FindGameObjectsWithTag("Bricks").Length + GameObject.FindGameObjectsWithTag("NewBallBrick").Length;
+                    if (totalBricks <= 0)
+                    {
+                        break;
+                    }
+                    GameObject bclone = Instantiate(BallClones, currentPos, resetPos.rotation);
+                    bclone.GetComponent<Rigidbody2D>().gravityScale = 0f;
+                    bclone.GetComponent<CloneBall>().speed = speed;
+                    bclone.GetComponent<CloneBall>().SendBallInDirection(n);
+                    yield return new WaitForSeconds(0.05f);
+                }
+
+
+            }
+
+            private void SendBallInDirection(Vector3 dir)
+            {
+                sprite.enabled = false;
+                touchedFloor = false;
+                rigid.gravityScale = .8f;
+                rigid.velocity = dir * speed;
+
+            }
+
+            private void TouchFloor()
+            {
+                sprite.enabled = true;
+                rigid.velocity = Vector2.zero;
+                rigid.gravityScale = 0;
+                touchedFloor = true;
+                Vector3 temp = transform.position;
+                temp.y = -1.606f;
+                transform.position = temp;
+
             }
 
 
-            //Currently dragging
-
-            //ballsPreview.localScale = Vector3.Lerp(new Vector3(1, 3, 1), new Vector3(1, 10, 1), sd.magnitude / MAXIMUM_PULL);
-        }
-
-        //Debug.Log(MobileInput.Instance.swipeDelta);
-
-    }
-
-    IEnumerator  ballWait(Vector3 n) {
-        CurrentBalls = GameController.currentBalls;
-        for (int i = 0; i < GameController.currentBalls; i++)
-        {
-            int totalBricks = GameObject.FindGameObjectsWithTag("Bricks").Length + GameObject.FindGameObjectsWithTag("NewBallBrick").Length;
-            if (totalBricks <= 0) {
-                break;
+            void Touched()
+            {
+                touchedFloor = true;
             }
-            GameObject bclone = Instantiate(BallClones, currentPos, resetPos.rotation);
-            bclone.GetComponent<Rigidbody2D>().gravityScale = .1f;
-            bclone.GetComponent<CloneBall>().speed = speed;
-            bclone.GetComponent<CloneBall>().SendBallInDirection(n);
-            yield return new WaitForSeconds(0.05f);
-        }
-
-        
-    }
-
-    private void SendBallInDirection(Vector3 dir)
-    {
-        sprite.enabled = false;
-        touchedFloor = false;
-        rigid.gravityScale = .8f;
-        rigid.velocity = dir * speed;
-
-    }
-
-    private void TouchFloor()
-    {
-        sprite.enabled = true;
-        rigid.velocity = Vector2.zero;
-        rigid.gravityScale = 0;
-        touchedFloor = true;
-        Vector3 temp = transform.position;
-        temp.y = -1.606f;
-        transform.position = temp;
-
-    }
 
 
-    void Touched() {
-        touchedFloor = true;
-    }
-
-
-   // void OnCollisionEnter2D(Collision2D collision)
-    //{
-        //if (collision.gameObject.tag == "Floor")
-       // {
+            // void OnCollisionEnter2D(Collision2D collision)
+            //{
+            //if (collision.gameObject.tag == "Floor")
+            // {
             //TouchFloor();
 
             //Debug.Log("This is working");
-       // }
- //   }
+            // }
+            //   }
 
-}
+        }
+    
