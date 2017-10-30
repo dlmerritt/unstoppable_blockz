59511feeb19fc898112ff9c7677206e4108dff12
Fernando de Paz <fdepaz321@gmail.com>
2017-10-30 00:49:54 -0700

    Ball Animation
    Ball now has a cool animation to it. Lots of mathfs involved.

---
+++ Prototype/Assets/Blink.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour {
+
+    public float BlinkSpeed = .5f;
+    private float currentTime;
+    private int currentBall;
+	// Use this for initialization
+	void Start () {
+        currentTime = 0;
+        currentBall = 0;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.childCount > 0)
+        {
+            if (currentTime <= BlinkSpeed)
+            {
+                currentTime += Time.deltaTime;
+            }
+            else
+            {
+                if (currentBall > transform.childCount - 1)
+                {
+                    currentBall = 0;
+                }
+                transform.GetChild(currentBall).GetComponent<SpriteRenderer>().color = Color.blue ;
+                currentBall++;
+                if (currentBall > transform.childCount - 1)
+                {
+                    currentBall = 0;
+                }
+                transform.GetChild(currentBall).GetComponent<SpriteRenderer>().color = Color.red;
+                currentTime = 0;
+            }
+        }
+		
+	}
+}
---
+++ Prototype/Assets/lineMake.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineMake : MonoBehaviour
+{
+    LineRenderer lr;
+    Vector3[] lines;
+    public float radius = 3;
+    public GameObject LinePart;
+    // Use this for initialization
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (lr.positionCount > 0)
+        {
+            if (lr.enabled)
+            {
+
+                lines = new Vector3[lr.positionCount];
+                lr.GetPositions(lines);
+                int removedLines = lr.positionCount - 1;
+                for (int i = removedLines; i < transform.childCount ; i++) {
+                    for (int j = 0; j < transform.GetChild(i).childCount; j++) {
+                        Destroy(transform.GetChild(i).GetChild(j).gameObject);
+                    }
+                }
+                for (int j = 0; j < lr.positionCount - 1; j++)
+                {
+                    float dist = Vector3.Distance(lines[j], lines[j+1]);
+
+                    int amount = (int)(dist / radius);
+
+                    if (transform.GetChild(j).childCount < amount)
+                    {
+                        int needed = amount - transform.GetChild(j).childCount;
+                        for (int i = 0; i < needed; i++)
+                        {
+                            GameObject spawned = (GameObject)Instantiate(LinePart, transform.position, transform.rotation);
+                            spawned.transform.SetParent(transform.GetChild(j));
+                        }
+                    }
+                    else if (transform.GetChild(j).childCount > amount)
+                    {
+                        int over = transform.GetChild(j).childCount - amount;
+                        for (int i = 0; i < over; i++)
+                        {
+                            Destroy(transform.GetChild(j).GetChild(transform.GetChild(j).childCount - i - 1).gameObject);
+                        }
+
+                    }
+
+                    float f = 0;
+                    for (int i = 0; i < transform.GetChild(j).childCount; i++)
+                    {
+                        Transform spawned = transform.GetChild(j).GetChild(i);
+                        spawned.position = Vector3.Lerp(lines[j], lines[j+1], f / dist);
+                        f += radius;
+                    }
+
+                }
+
+
+            }
+            else {
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    for (int j = 0; j < transform.GetChild(i).childCount; j++)
+                    {
+                        Destroy(transform.GetChild(i).GetChild(j).gameObject);
+                    }
+                }
+            }
+
+        }
+
+
+
+
+    }
+}
