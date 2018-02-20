9cb4c377ae8df9e8d4aa84ffb2d5bedb459b4362
dlmerritt <dlmerrit@uci.edu>
2018-02-19 17:39:02 -0800

    Shop Updates
    midterms done... updates coming

--- Prototype/Assets/Scripts/ShopMenuController.cs
+++ Prototype/Assets/Scripts/ShopMenuController.cs
@@ -10,5 +10,27 @@ public class ShopMenuController : MonoBehaviour {
         SceneManager.LoadScene("menuScene");
     }
 
+	public void OnRedBallsClicked() 
+	{
+
+		Debug.Log ("Red Balls purchased!");
+
+	}
+
+	public void OnBlueBallsClicked() 
+	{
+
+		Debug.Log ("Blue Balls purchased!");
+
+	}
+
+	public void OnGreenBallsClicked() 
+	{
+
+		Debug.Log ("Green Balls purchased!");
+
+	}
+
+
 
 }
